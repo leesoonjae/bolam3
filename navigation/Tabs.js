98c1509540186a9b3cfad328f4c6ee260b6c688c
Kidongg <dlrlehd7@daum.net>
2023-01-06 21:57:17 +0900

    ✅ Add Tabs.js
    탭 네비게이터를 만들고 Main, Reading, Finished 스크린을 만들었습니다.

--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -1,10 +1,24 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Main from "../screens/Main";
+import Reading from "../screens/Reading";
+import Finished from "../screens/Finished";
+import styled from "@emotion/native";
+
+const Tab = createBottomTabNavigator();
 
 export default function Tabs() {
   return (
-    <View>
-      <Text>Tabs입니다</Text>
-    </View>
+    <Tab.Navigator
+      screenOptions={{ headerTitleAlign: "center" }}
+      sceneContainerStyle={{
+        paddingLeft: 30,
+        paddingRight: 30,
+      }}
+    >
+      <Tab.Screen name="Main" component={Main} />
+      <Tab.Screen name="Reading" component={Reading} />
+      <Tab.Screen name="Finished" component={Finished} />
+    </Tab.Navigator>
   );
 }
