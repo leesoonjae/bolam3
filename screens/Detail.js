4fbb2e4992e794dbac8940a5d37ca8367158b602
Kidongg <dlrlehd7@daum.net>
2023-01-06 21:58:59 +0900

    ✅ Add Seed File
    베이스 코드 파일을 만들었습니다.

---
+++ screens/Detail.js
@@ -0,0 +1,10 @@
+import react from "react";
+import { Text, View } from "react-native";
+
+export default function Detail() {
+  return (
+    <View>
+      <Text>Detail 페이지 입니다.</Text>
+    </View>
+  );
+}
--- screens/Main.js
+++ screens/Main.js
@@ -1,12 +1,32 @@
-// import React from "react";
-// import { View, Text } from "react-native";
+import React from "react";
+import { Text, TouchableOpacity } from "react-native";
 
-// function Main({ navigation }) {
-//   return (
-//     <View>
-//       <Text>Main</Text>
-//     </View>
-//   );
-// }
+function Main({ navigation: { navigate } }) {
+  const goAdd = () => {
+    navigate("Stacks", { screen: "Add" });
+  };
 
-// export default Main;
+  const goDetail = () => {
+    navigate("Stacks", { screen: "Detail" });
+  };
+
+  const goDetailEdit = () => {
+    navigate("Stacks", { screen: "DetailEdit" });
+  };
+
+  return (
+    <>
+      <TouchableOpacity onPress={goAdd}>
+        <Text>AddAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={goDetail}>
+        <Text>Detail</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={goDetailEdit}>
+        <Text>Detail Edit</Text>
+      </TouchableOpacity>
+    </>
+  );
+}
+
+export default Main;
