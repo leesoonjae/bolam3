c3045772b11b94574fa8bd1e00fb77d743dd6e71
Kidongg <dlrlehd7@daum.net>
2023-01-06 21:56:09 +0900

    ✅  Add Root.js
    루트 파일에 네비게이터를 만들고 Tabs와 Stacks 스크린을 만들었습니다

--- navigation/Root.js
+++ navigation/Root.js
@@ -7,8 +7,14 @@ const Stack = createStackNavigator();
 
 export default function Root() {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      {/* 메인, 완독, 읽고있는 책 페이지  =Tabs */}
       <Stack.Screen name="Tabs" component={Tabs} />
+      {/* 상세페이지, 작성페이지 =Stacks */}
       <Stack.Screen name="Stacks" component={Stacks} />
     </Stack.Navigator>
   );
