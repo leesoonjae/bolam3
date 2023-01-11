c2385176747757173a13e4e8b2eb540f6d1f5472
Kidongg <dlrlehd7@daum.net>
2023-01-11 16:19:00 +0900

    🔧 Refactoring to Redux-thunk
    리덕스 떵크를 사용해 추가 페이지 데이터를 스토어에 저장하고 서버에 추가시켰습니다.

---
+++ redux/config/configStore.js
@@ -0,0 +1,24 @@
+// import { createStore, combineReducers } from "redux";
+// import readMe from "../modules/readMe";
+
+// rootReducer
+// const rootReducer = combineReducers({
+//   readMe: readMe,
+// });
+
+// store
+// const store = createStore(rootReducer);
+
+// export default store;
+
+/////////////// 리덕스와 리덕스 툴킷 구분 ///////////////
+
+import { configureStore } from "@reduxjs/toolkit";
+import readMe from "../modules/readMe";
+
+// root reducer, store
+const store = configureStore({
+  reducer: { readMe, readMe },
+});
+
+export default store;
