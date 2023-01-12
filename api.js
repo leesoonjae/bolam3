7e6c5ef2c58c3b9a2a1c142d610707d34dcae2e7
SoonJae Lee <117057864+leesoonjae@users.noreply.github.com>
2023-01-12 19:01:25 +0900

    feet: 삭제 수정 기능 구현완료

--- api.js
+++ api.js
@@ -1,9 +1,20 @@
+// import axios from "axios";
+
 import axios from "axios";
 
-export const getDataFromServer = async () => {
-  const response_data = await axios.get("http://172.30.1.39:4000/test");
-  return response_data.data;
+// export const getDataFromServer = async () => {
+//   const response_data = await axios.get("http://172.30.1.39:4000/test");
+//   return response_data.data;
+// };
+
+const API_KEY = "ttbsoojae10291105001";
+
+export const getBooks = () => {
+  const bookApi = axios.get(
+    `http://www.aladin.co.kr/ttb/api/ItemList.aspx?ttbkey=ttbsoojae10291105001&QueryType=BlogBest&MaxResults=1&start=1&SearchTarget=Book&output=JS&Version=20131101`
+  );
+  console.log(bookApi);
+  const aaa = JSON.parse(bookApi.request._response);
+  return aaa.then((res) => res.json());
 };
-
-
-
+// const aaa = JSON.parse(bookApi.request._response);
